ec51bf078d1ed3f5585cd8e8c259e39578c66454
Polina <ustinovapolina1@gmail.com>
2021-12-15 07:51:20 +0100

    made a code for the homework 3

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from phones.models import Phone
 
 
 def index(request):
@@ -7,11 +8,20 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sort_by = request.GET.get('sort')
+    all_phones_we_have = Phone.objects.all()
+    if sort_by == 'low':
+        all_phones_we_have = all_phones_we_have.order_by('price')
+    elif sort_by == 'high':
+        all_phones_we_have = all_phones_we_have.order_by('-price')
+    elif sort_by == 'alphabet':
+        all_phones_we_have = all_phones_we_have.order_by('name')
+    context = {'phone': all_phones_we_have}
+    return render(request, template, context = context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = Phone.objects.filter(slug__contains=slug)
+    context = {'phone': phone}
+    return render(request, template, context = context)
